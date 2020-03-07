556049736cc3667884cc9e0222cd220a70147526
Llamalover217 <42660246+Llamalover217@users.noreply.github.com>
2020-03-08 01:29:37 +1300

    added more stuff
    added more stuff

--- guest book/guestbook.py
+++ guest book/guestbook.py
@@ -3,7 +3,7 @@ import sqlite3
 
 app = Flask(__name__)
 
-DATABASE = 'mini.db'  
+DATABASE = 'shopping.db'  
 
 def get_db():   # This connects the database to my website
     db = getattr(g, '_database', None)
@@ -17,28 +17,40 @@ def close_connection(exception):    # this closes the database is the website is
     if db is not None:
         db.close()
 
-@app.route('/index', methods=['GET', 'POST'])
-def index():    # this gets information from another file and brings it up
-    return render_template('index.html')   # it tells the computer what the file is called that it needs to bring up
-
 @app.route('/')
 def home():    # this gets information from the home page file and brings it up
     cursor = get_db().cursor()     # this gets the database to be connected to the website
     sql = "SELECT * FROM shopping_cart"
     cursor.execute(sql)
     results = cursor.fetchall()
-    return render_template('home.html', results = results)    # it tells the computer what the file is called that it needs to bring up
+    return render_template('home.html', results=results)    # it tells the computer what the file is called that it needs to bring up
 
-@app.route('/add', methods=["GET", "POST"])
+@app.route('/add', methods=['GET', 'POST'])
 def add():
+    print("hi")
     if request.method == "POST":
         cursor = get_db().cursor()     # this gets the database to be connected to the website
-        new_name = request.form["item"]
-        new_cost = request.form["cost"]
-        sql = "INSERT INTO shopping_cart(item,cost) VALUES (?,?)"
+        new_name = request.form["item_name"]
+        new_cost = request.form["item_cost"]
+        print(new_name, new_cost)
+        sql = "INSERT INTO shopping_cart(item,cost) VALUES(?,?)"
         cursor.execute(sql,(new_name, new_cost))
         get_db().commit()
     return redirect('/')
+
+@app.route('/delete', methods=['GET', 'POST'])
+def delete():
+    if request.method == "POST":   # get the item and delete from database
+        cursor = get_db().cursor()
+        id = int(request.form["item_name"])
+        sql = "DELETE FROM shopping_cart WHERE id=?"
+        cursor.execute(sql,(id,))
+        get_db().commit()
+    return redirect('/')
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():    # this gets information from another file and brings it up
+    return render_template('index.html')   # it tells the computer what the file is called that it needs to bring up
 
 @app.route('/example', methods=['GET', 'POST'])
 def example():    # this is an example page I made and kept on here as a little secret for people to find if they wish to do so
